f373546bed406f2879a38d65597808d7b4591852
RyanHan0127 <ryanhanboy@gmail.com>
2021-03-22 12:28:10 -0700

    Fixed file security issue

--- bot.py
+++ bot.py
@@ -1,12 +1,32 @@
 import discord
 import logging
+import os
+from dotenv import load_dotenv
 from discord.ext import commands
 
-TOKEN = "秘密です( ͡❛ ω ͡❛)"
+load_dotenv()
 BOT_PREFIX = ("!")
-
+TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(BOT_PREFIX)
 
-@bot.command()
-async def vibecheck(ctx, arg):
-    pass
+@bot.command(name = 'vibecheck', pass_context=True)
+async def vibecheck(ctx, *arg):
+	current_channel_id = ctx.message.channel.id
+	user_id = ctx.message.author.id
+	print(user_id)
+	channel = bot.get_channel(current_channel_id)
+	messages = await ctx.channel.history(limit=200).flatten()
+
+	for msg in messages:
+		if msg.author.id == user_id:
+			print(msg.content + " : " + msg.created_at.strftime("%d/%m/%Y, %H:%M:%S"))
+	#await ctx.send("Vibe Check")
+
+@bot.event
+async def on_ready():
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
+
+bot.run(TOKEN)
