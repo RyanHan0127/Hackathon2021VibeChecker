a6a6fb94d9981ab4e30503af81fc87c9c010eeb7
mdelfincameron <mdelfincameron@users.noreply.github.com>
2021-03-22 00:34:34 -0700

    Added basic function/variables

--- bot.py
+++ bot.py
@@ -1,1 +1,12 @@
 import discord
+import logging
+from discord.ext import commands
+
+TOKEN = "秘密です( ͡❛ ω ͡❛)"
+BOT_PREFIX = ("!")
+
+bot = commands.Bot(BOT_PREFIX)
+
+@bot.command()
+async def vibecheck(ctx, arg):
+    pass
