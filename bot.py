93c3b872b7d2c240c13d3f08de7f1447df909a6e
mdelfincameron <mdelfincameron@users.noreply.github.com>
2021-03-23 02:21:51 -0700

    Added error checking for both an integer and a mention argument

--- bot.py
+++ bot.py
@@ -7,10 +7,32 @@ from discord.ext import commands
 load_dotenv()
 BOT_PREFIX = ("!")
 TOKEN = os.getenv('DISCORD_TOKEN')
+MSG_LIMIT = 100
 bot = commands.Bot(BOT_PREFIX)
 
 @bot.command(name = 'vibecheck', pass_context=True)
 async def vibecheck(ctx, arg):
+
+    amt = 100      #Default/placeholder, idk if we want to do a mention-based one as well but w/e I added error checking for it anyway
+    isValid = true
+    if not ctx.message.mentions:
+        print("Is not a mention")
+        try:
+            amt = int(arg)
+        except ValueError:
+            isValid = False
+        if not isValid:
+            print("Not an integer")
+            await ctx.send("Argument passed was not an integer or a mention")
+            return
+        else:
+            print("Is an integer")
+    else:
+        #Check for the first message sent by mentioned user in an arbitrary time period and adjust amt accordingly
+        #or something idk
+        print("Is a mention")
+
+
 	current_channel_id = ctx.message.channel.id
 	user_id = ctx.message.author.id
 	print(user_id)
